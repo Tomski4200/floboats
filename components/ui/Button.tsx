34c7950c8fa672e27638a94bfd1a11eeaf066639
Vercel Deploy <deploy@vercel.com>
2025-08-02 21:41:06 -0400

    Fix circular import in Button.tsx - point to button-component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,2 +1,2 @@
-// Lowercase redirect file for case-sensitive systems
-export { Button, type ButtonProps } from './Button'
+// Redirect file for case-sensitive systems
+export { Button, type ButtonProps } from './button-component'
